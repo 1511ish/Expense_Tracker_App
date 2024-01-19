1ad705d023e25914487978705642f4bdb22a22db
1511ish <117707870+1511ish@users.noreply.github.com>
2024-01-19 14:21:56 +0530

    dyanmic pagination

--- public/index.js
+++ public/index.js
@@ -8,13 +8,103 @@ let expence_amount = document.getElementById('amount');
 let description = document.getElementById('description');
 const buyPremium_btn = document.getElementById('rzp-button1');
 const show_leaderboard_btn = document.getElementById('show_leaderboard');
-const download_btn = document.getElementById('downloadexpense');
+const download_btn = document.getElementById('downloadexpense')
+let pagination = document.getElementById('pagination');
 const token = localStorage.getItem('token');
 
 addExpense_btn.addEventListener('click', addExpense);
 buyPremium_btn.addEventListener('click', buyPremium);
 show_leaderboard_btn.addEventListener('click', showLeaderBoard)
 download_btn.addEventListener('click', download);
+
+const currentPage = 1;
+let itemsPerPage = 1;
+if(localStorage.getItem('rowPerPage')){
+    itemsPerPage = localStorage.getItem('rowPerPage');
+    // console.log(typeof(itemsPerPage));
+}
+
+
+function showPremiumUserMessage() {
+    document.getElementById('rzp-button1').style.visibility = "hidden";
+    document.getElementById('message').innerHTML = "You are a premium user.";
+    document.getElementById('show_leaderboard').style.visibility = "visible";
+    document.getElementById('leaderboard').style.visibility = "visible";
+}
+
+
+function parseJwt(token) {
+    var base64Url = token.split('.')[1];
+    var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+    var jsonPayload = decodeURIComponent(window.atob(base64).split('').map(function (c) {
+        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+    }).join(''));
+
+    return JSON.parse(jsonPayload);
+}
+
+
+window.addEventListener("DOMContentLoaded", () => {
+    const token = localStorage.getItem('token')
+    const decodeToken = parseJwt(token);
+    console.log(decodeToken);
+    const isPremiumUser = decodeToken.isPremium;
+    if (isPremiumUser) {
+        showPremiumUserMessage();
+    }
+    getExpenses(1);
+
+    // axios.get('http://localhost:3000/user/get-downlodedFileUrls', { headers: { 'Authorization': token } })
+    //     .then(response => {
+    //         console.log(response.data.fileURL);
+    //         for (var i = 0; i < response.data.fileURL.length; i++) {
+    //             showDownloadedFile(response.data.fileURL[i])
+    //         }
+    //     })
+    //     .catch(err => console.log(err));
+})
+
+
+function showPagination({ currentPage, hasNextPage, nextPage, hasPreviousPage, previousPage, lastPage }) {
+    pagination.innerHTML = '';
+
+    if (hasPreviousPage) {
+        const btn2 = document.createElement('button');
+        btn2.innerHTML = previousPage;
+        btn2.addEventListener('click', () => getExpenses(previousPage));
+        pagination.appendChild(btn2);
+    }
+    const btn1 = document.createElement('button');
+    btn1.innerHTML = `<h3>${currentPage}</h3>`
+    btn1.addEventListener('click', () => getExpenses(currentPage))
+    pagination.appendChild(btn1);
+
+    if (hasNextPage) {
+        const btn3 = document.createElement('button');
+        btn3.innerHTML = nextPage;
+        btn3.addEventListener('click', () => getExpenses(nextPage))
+        pagination.appendChild(btn3);
+    }
+}
+
+
+function listExpenses(allExpense) {
+    for (var i = 0; i < allExpense.length; i++) {
+        showDataOnScreen(allExpense[i])
+        // console.log(allExpense);
+    }
+}
+
+
+function showDownloadedFile(obj) {
+    expensefile_ul.innerHTML = expensefile_ul.innerHTML + `<li><a href= ${obj.fileUrl}>Expense file ${obj.id}</a></li>`;
+}
+
+
+function showDataOnScreen(expense) {
+    ul.innerHTML = ul.innerHTML + `<li id="${expense.id}"> ${expense.expense_amount} - ${expense.description} - ${expense.category} - <button onclick="editExpense('${expense.id}','${expense.expense_amount}','${expense.description}','${expense.category}')">Edit</button> <button onclick="deleteExpense(${expense.id})">Delete</button></li>`;
+}
+
 
 function addExpense() {
     let ind = document.getElementById("options").selectedIndex;
@@ -40,66 +130,13 @@ function addExpense() {
 }
 
 
-function showPremiumUserMessage() {
-    document.getElementById('rzp-button1').style.visibility = "hidden";
-    document.getElementById('message').innerHTML = "You are a premium user.";
-    document.getElementById('show_leaderboard').style.visibility = "visible";
-    document.getElementById('leaderboard').style.visibility = "visible";
-}
-
-
-function parseJwt(token) {
-    var base64Url = token.split('.')[1];
-    var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-    var jsonPayload = decodeURIComponent(window.atob(base64).split('').map(function (c) {
-        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
-    }).join(''));
-
-    return JSON.parse(jsonPayload);
-}
-
-
-let currentPage = 1;
-let itemsPerPage = 5;
-let pre_btn;
-let next_btn
-window.addEventListener("DOMContentLoaded", () => {
-    console.log("reload");
-
-    const token = localStorage.getItem('token')
-    const decodeToken = parseJwt(token);
-    const isPremiumUser = decodeToken.isPremium;
-    if (isPremiumUser) {
-        showPremiumUserMessage();
-    }
-    axios.get(`http://localhost:3000/expense/get-expenses?page=${currentPage}&pageSize=${itemsPerPage}`, { headers: { 'Authorization': token } })
-        .then(response => {
-            for (var i = 0; i < response.data.allExpense.length; i++) {
-                showDataOnScreen(response.data.allExpense[i])
-            }
-            pre_btn = document.createElement('button');
-            pre_btn.value = currentPage;
-            next_btn = document.createElement('button');
-            next_btn.value = currentPage+1;
+function getExpenses(page) {
+    ul.innerHTML = '';
+    axios.get(`http://localhost:3000/expense/get-expenses?page=${page}&pageSize=${itemsPerPage}`, { headers: { 'Authorization': token } })
+        .then(({ data: { allExpenses, ...pageData } }) => {
+            listExpenses(allExpenses);
+            showPagination(pageData);
         })
-        .catch(err => console.error(err));
-    axios.get('http://localhost:3000/user/get-downlodedFileUrls', { headers: { 'Authorization': token } })
-        .then(response => {
-            console.log(response.data.fileURL);
-            for (var i = 0; i < response.data.fileURL.length; i++) {
-                showDownloadedFile(response.data.fileURL[i])
-            }
-        })
-        .catch(err => console.log(err));
-})
-
-
-function showDownloadedFile(obj) {
-    expensefile_ul.innerHTML = expensefile_ul.innerHTML + `<li><a href= ${obj.fileUrl}>Expense file ${obj.id}</a></li>`;
-}
-
-function showDataOnScreen(expense) {
-    ul.innerHTML = ul.innerHTML + `<li id="${expense.id}"> ${expense.expense_amount} - ${expense.description} - ${expense.category} - <button onclick="editExpense('${expense.id}','${expense.expense_amount}','${expense.description}','${expense.category}')">Edit</button> <button onclick="deleteExpense(${expense.id})">Delete</button></li>`;
 }
 
 
@@ -196,3 +233,11 @@ function download(e) {
         })
 }
 
+function updateRowsPerPage() {
+    // e.preventDefault();
+    console.log("running");
+    const index = document.getElementById('rowsPerPage').selectedIndex;
+    const rowsPerPage = document.getElementsByClassName('rows')[index].value;
+    localStorage.setItem('rowPerPage', rowsPerPage);
+    location.reload();
+}
